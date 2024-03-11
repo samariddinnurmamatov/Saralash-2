4ab2a5e54d70984baa9a2f0e251425765c1277c6
samariddinnurmamatov <samariddinnurmamatov111@gmail.com>
2024-03-11 20:36:25 +0500

    React E-commerce

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import './index.css'
 import store from './redux/store/store.jsx';
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
